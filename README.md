091f6edf7baabfc1632d84500c606c66ebfe3fdb
David McFarlane <d-mcfarlane@live.com>
2024-05-24 00:52:34 -0300

    Adding script and README

---
+++ README.md
@@ -0,0 +1,25 @@
+# C is for Coffee - A Network Chuck Inspired Project
+
+Copied what Network Chuck did in his [**video**](https://www.youtube.com/watch?v=nTqu6w2wc68&ab_channel=NetworkChuck)... because I like coffee too!
+
+Another simple little Clang program that prints out a message to the console. This time, it's about coffee!
+
+## How to Run
+1. Clone this repo
+2. Navigate to the directory where you cloned the repo
+3. Run the following commands:
+```bash 
+clang coffee.c -o coffee
+./coffee
+```
+
+## Sample Output
+```bash
+I've had 20 coffees today! (c to increment)
+I've had 7 servings of water today! (h to increment)
+I've had 1 bowls of oatmeal today! (o to increment)
+I've had 5 walks today! (w to increment)
+I've had 21 pushups today! (p to increment)
+I've had 8 stretches today! (s to increment)
+```
+
---
+++ coffeeCounter.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int coffeeCount; 
+int waterCount; 
+int oatmealCount; 
+int walkCount; 
+int pushUpCount; 
+int stretchCount; 
+
+bool updateFlag;
+bool done;
+
+/*void Increment()
+{
+    coffeeCount++; 
+}
+
+void PrintCoffeeCount()
+{
+    printf("I've had %d coffees today!", coffeeCount);
+}*/
+
+void DoNewLine()
+{
+    printf("\n");
+}
+
+void RevisedPrintCounts(int amount, char *name, char *incrementChar)
+{
+    printf("I've had %d %s today! (%s to increment)\n", amount, name, incrementChar);
+}
+
+void PrintCounts()
+{
+    RevisedPrintCounts(coffeeCount, "coffees", "c");
+    RevisedPrintCounts(waterCount, "servings of water", "h");
+    RevisedPrintCounts(oatmealCount, "bowls of oatmeal", "o"); 
+    RevisedPrintCounts(walkCount, "walks", "w");
+    RevisedPrintCounts(pushUpCount, "pushups", "p");
+    RevisedPrintCounts(stretchCount, "stretches", "s");
+}
+
+void PrintOptions()
+{
+    printf("c to increment coffee\n");
+    printf("s to increment stretches\n");
+    printf("o to increment oatmeal\n");
+    printf("h to increment water (h20)\n");
+    printf("w to increment walks\n");
+    printf("p to increment pushups\n");
+}
+
+bool ApplyCount(char charVal)
+{
+    bool result = true;
+    switch (charVal) 
+    {
+        case 'c':  // coffee 
+            coffeeCount++;
+            break; 
+        case 'h': // water
+            waterCount++; 
+            break;
+        case 'w':
+            walkCount++; // walks
+            break; 
+        case 's': // stretch
+            stretchCount++;
+            break;
+        case 'p': // pushup
+            pushUpCount++; 
+            break; 
+        case 'o': // oatmeal
+            oatmealCount++;
+            break;
+        default:
+            result = false;
+            break;
+    }
+    return result; 
+}
+
+int main() {
+
+    coffeeCount = 0; 
+    done = false; 
+
+    char *line = NULL;
+    size_t len = 0;
+
+    do 
+    {
+        PrintCounts();
+        getline(&line, &len, stdin);
+
+        if (strlen(line) > 0)
+        {
+            char c = line[0];
+
+            if (!ApplyCount(c))
+            {
+                done = true;
+            }
+        }
+        printf("\e[1;1H\e[2J");
+    } while (!done);
+
+    return 0;
+}
